78c2d614d14730cba0544bccda76851b7cb280e7
Siraj <sirajchokshi@gmail.com>
2019-10-07 11:16:45 -0500

    Add button styles to listings, replace static info with temp variables

--- frontend/src/components/Listing.js
+++ frontend/src/components/Listing.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 // Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBookmark as bookmarkActive } from '@fortawesome/free-solid-svg-icons'
-import { faBookmark as bookmark } from '@fortawesome/free-regular-svg-icons'
+import { faBookmark as bookmarkIcon, faExternalLinkAlt as appIcon, faMapMarkerAlt as locationIcon } from '@fortawesome/free-solid-svg-icons'
+import { faClock as deadlineIcon } from '@fortawesome/free-regular-svg-icons'
 
-var image = "https://via.placeholder.com/200x200";
+var logo = "https://via.placeholder.com/200x200";
+var bookmarked = false;
+var bookmarkButtonClass = "bookmark button-secondary";
+var jobPage = "/";
+var deadline = "November 2nd";
+var location = "Champaign IL";
+var company = "StartupName";
+var jobName = "Job Title Here Internship Summer 2020";
+var shortDesc = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+
+if (bookmarked) {
+  bookmarkButtonClass = "button-secondary bookmarked";
+}
 
 export default class Listing extends Component {
   state = {
@@ -14,13 +28,28 @@ export default class Listing extends Component {
   render () {
       return (
         <div className="listing">
-          <div className="image-wrapper"><img src={image}></img></div>
+          <Link className="image-wrapper"><img src={logo}></img></Link>
           <div className="inf">
-            <h2>Listing Title <span className="commitment-wrapper"> &mdash; <span className="commitment">Full-time</span></span></h2>
-            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <Link to={jobPage}><h2>{jobName} | {company}</h2></Link>
+            <p className="sub-title">
+              <FontAwesomeIcon icon={locationIcon}></FontAwesomeIcon>
+              &nbsp; {location} &nbsp; &mdash; &nbsp;&nbsp;
+              <FontAwesomeIcon icon={deadlineIcon}></FontAwesomeIcon>
+              &nbsp; {deadline}
+            </p>
+            <p>
+              {shortDesc}
+              <Link to="jobPage">read more...</Link>
+            </p>
             <div className="button-wrapper">
-              <a href="" className="button">Apply</a>
-              <a href="" className="button-secondary">Bookmark</a>
+              <Link to={jobPage} className="button">
+                Apply &nbsp;
+                <FontAwesomeIcon icon={appIcon}></FontAwesomeIcon>
+              </Link>
+              <button className={bookmarkButtonClass}>
+                <FontAwesomeIcon icon={bookmarkIcon}></FontAwesomeIcon>
+                &nbsp; Bookmark
+              </button>
               <a href="" className="similar-pos">Similar positions...</a>
             </div>
           </div>
